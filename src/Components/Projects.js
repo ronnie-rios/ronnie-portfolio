483ae6e2d4975e4ef3fe8f61c80ad5fd137502ef
Ronnie Rios <ronniesmail14@gmail.com>
2023-02-19 13:59:26 -0600

    fix imgs

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,26 +9,31 @@ const Projects = () => {
                 <div className="flex flex-col w-full mb-20">
                     <CodeIcon className="mx-auto inline-block w-10 mb-4" />
                     <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-                        Projects I have worked on
+                        Projects I Have Worked On
                     </h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                        Click the box to viewed the deploy application. The link to the repository is alos listed at the bottom..
+                        Hover over the image to see more details of the project. 
+                    </p>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                        Click the box to viewed the deploy application.
+                    </p>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                        The link to the repository is listed at the bottom.
                     </p>
                 </div>
-                <div className="flex flex-wrap -m-4">
+                <div className="flex flex-wrap m-4">
                     {projects.map((project) => (
                         <a
                             href={project.link} target="_blank"
                             key={project.title}
-                            className="sm:w-1/2 w-100 p-4"
-                            rel="noreferrer">
-                            <div className="flex relative rounded-md">
-                              {/* <img
+                            className="sm:w-1/2 w-100 p-4">
+                            <div className="flex relative">
+                              <img
                                 alt="gallery"
-                                className="absolute inset-0 w-full h-full object-cover object-center rounded-md"
+                                className="absolute inset-0 w-full h-full object-scale-down"
                                 src={project.image}
-                              /> */}
-                        <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 hover:opacity-100 rounded-md">
+                              />
+                        <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                         <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                             {project.tech}
                         </h2>
--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+import jobtrackr from './assets/images/jobtrackr.png'
+import ghfindr from './assets/images/githubfind.png'
 export const projects = [
   {
       title: 'Job tracker',
@@ -5,31 +7,15 @@ export const projects = [
       description: 'A full stack application where a user can create account and add different jobs they are applying to and update them.',
       link: 'https://job-tracker-r56d.onrender.com/',
       github: 'https://github.com/ronnie-rios/app-tracker',
-      image: ''
+      image: jobtrackr
   },
-//   {
-//     title: 'Beyond Us: MERN Application',
-//     tech: 'JavaScript, Node.js, MongoDB, Mongoose, Express, React',
-//     description: 'A full stack application that allows users to create accounts, and create posts for different causes that are impacting the world. ',
-//     link: 'https://young-taiga-79587.herokuapp.com/',
-//     github: 'https://github.com/ronnie-rios/beyond-us',
-//     image: '../assets/images/beyond-us.png'
-// },
-//   {
-//       title: 'Shop-Shop Redux',
-//       tech: 'JavaScript, Node.js, MongoDB, Mongoose, Express, React',
-//       description: 'This is a fictional e-commerce application that uses React-Redux for  global state control. Users can create an account and add items to their cart.',
-//       link: 'https://obscure-sands-72753.herokuapp.com/',
-//       github: 'https://github.com/ronnie-rios/shop-shop-redux',
-//       image: '../assets/images/shopshopredux.png'
-//   },
   {
     title: 'Github Finder',
     tech: 'React, JavaScript, Tailwind CSS, Daisy UI, Github API',
     description: `This is a React application that allows a user to search for profiles via the Github API. The user can click on a profile to view the selected github profile.`,
     link: 'https://github-finder-lyart-xi.vercel.app/',
     github: 'https://github.com/ronnie-rios/github-finder',
-    image: '../assets/images/githubfind.png'
+    image: ghfindr
   }
 
 ];
